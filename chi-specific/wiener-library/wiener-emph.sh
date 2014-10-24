f25229c56275acbacde60051589397679ff7ccb3
bencomp <ben@companjen.name>
2014-10-24 18:30:33 +0200

    Add a (fragile) script to run all preprocessor tools

--- chi-specific/wiener-library/wiener-emph.sh
+++ chi-specific/wiener-library/wiener-emph.sh
@@ -3,6 +3,6 @@
 # usage: ./wiener-emph.sh <saxon.jar> <input file folder/>
 
 for FILE in `ls $2`; do
-    echo "Transforming $FILE..."
+    echo "Removing emph from names in $FILE..."
     java -jar "$1" -xsl:wiener-emph.xsl -s:"$2/$FILE" -o:"$2-emph-removed/$FILE"
 done
